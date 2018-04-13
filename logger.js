0e169ef50e6bb377a56eb5de5a96d677200ef702
rsbabcock <rachael.s.babcock@gmail.com>
2018-04-13 07:58:20 -0500

    added first empty object for cities and empty array for cities, also changed main.js to logger.js

---
+++ logger.js
@@ -0,0 +1,12 @@
+
+// Create an application that displays 10 cities you have visited from a database (stored in local storage)
+// Details for each city
+// a. City name
+// b. Year you visited
+// c. Image of city
+// d. List of top 5 attractions
+
+const citiesDatabase = {}
+
+citiesDatabase.cities = []
+
